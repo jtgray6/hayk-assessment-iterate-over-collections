24791f0dc6bc685e19b62eb8bb64b32dea94ae63
Jeremy Gray <jeremytgray6@gmail.com>
2020-01-14 13:23:12 -0700

    completed requirements outlined in readme and cleaned up code

--- app.rb
+++ app.rb
@@ -1,3 +1,5 @@
+require 'pry'
+
 class Company
     attr_accessor :name, :size
 
@@ -9,8 +11,21 @@ end
 
 companies = [
     Company.new('Alpha', 30),
-    Company.new('Beta, 300),
-    Company.new('Delta, 3000)
+    Company.new('Beta', 300),
+    Company.new('Delta', 3000)
 ]
 
+companies.each {|company| puts "#{company.name} - #{company.size}"}
+
+sum_sizes = companies.map {|company| company.size}.reduce {|sum, size| sum+=size}
+
+over_100 = companies.select {|company| company.size > 100}
+
+beta_company = companies.find {|company| company.name == "Beta"}
+
+largest_company = companies.max_by {|company| company.size}
+
+sort_large_to_small = companies.sort_by {|company| company.size}.reverse
+
+binding.pry
 ### Your code below this
